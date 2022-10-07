6df5dae2d298dda19f00af441ce1b9ac1babb579
Neurological01 <113656521+Neurological01@users.noreply.github.com>
2022-10-07 16:46:03 +0800

    Create BloxFruits.lua

---
+++ BloxFruits.lua
@@ -0,0 +1,16 @@
+wait(1.2)
+game.StarterGui:SetCore("SendNotification", {
+Title = "Script made by Neuro";
+Text = "GUI is loading...";
+Duration = 4;
+})
+
+wait(2)
+game.StarterGui:SetCore("SendNotification", {
+Title = "Blox Fruits GUI loaded!";
+Text = "Have fun!";
+Duration = 4;
+})
+
+wait(6.4)
+game.Players.LocalPlayer:Kick("Cheating while an admin is in the game?")
